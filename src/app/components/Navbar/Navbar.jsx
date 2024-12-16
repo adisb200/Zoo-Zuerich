adb019a56fe9315ee4d7e53c1834472a72ab7145
adisb <adis.basovic@lernende.bbw.ch>
2024-12-16 11:12:09 +0100

    add logo and css

--- src/app/components/Navbar/Navbar.jsx
+++ src/app/components/Navbar/Navbar.jsx
@@ -4,18 +4,24 @@ import {useState} from "react";
 import Link from "next/link";
 import styles from "./Navbar.module.css";
 import MenuIcon from '@mui/icons-material/Menu';
+import Image from "next/image";
+import logoSvg from '../../../assets/images/logo.svg'
 
 export default function Navbar() {
     const [open, setOpen] = useState(false);
 
     return (
-        <div className={styles.flex}>
-            <MenuIcon onClick={() => setOpen(!open)}/>
+        <>
+            <div>
+                <MenuIcon onClick={() => setOpen(!open)}/>
+                <Image height={20} src={logoSvg} alt={'logo'}/>
+            </div>
 
-            {open && <div>
+
+            {open && <div className={'styles.flex'}>
                 <Link className={styles.link} href={"/"}>Tickets anzeigen</Link>
                 <Link className={styles.link} href={"/"}>Karten & Route</Link>
                 <Link className={styles.link} href={"/"}>Tickets kaufen</Link>
             </div>}
-        </div>)
+        </>)
 }
