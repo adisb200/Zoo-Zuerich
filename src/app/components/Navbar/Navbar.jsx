d11ef36d36c31375efa9f7e19810ef1c6dc9adce
adisb <adis.basovic@lernende.bbw.ch>
2025-01-12 16:17:12 +0100

    adjust height and navigation

--- src/app/components/Navbar/Navbar.jsx
+++ src/app/components/Navbar/Navbar.jsx
@@ -10,16 +10,18 @@ import logoSvg from '../../../assets/images/logo.svg'
 export default function Navbar() {
     const [open, setOpen] = useState(false);
 
-    const links = [{href: '/', label: 'Home'}, {href: '/pages/buyticket', label: 'Tickets anzeigen'}, {
-        href: '/TEST',
-        label: 'Karten & Route'
-    }, {href: '/TEST2', label: 'Tickets kaufen'}];
+    const links = [
+        {href: '/', label: 'Home'},
+        {href: '/pages/showticket', label: 'Tickets anzeigen'},
+        {href: '/pages/map', label: 'Karten & Route'},
+        {href: '/pages/buyticket', label: 'Tickets kaufen'}
+    ];
 
 
     return (<div className={styles.background}>
         <div style={{display: "flex", justifyContent: "space-between"}}>
-            <MenuIcon onClick={() => setOpen(!open)} style={{height: 40, width: 40}}/>
-            <Image height={40} src={logoSvg} alt={'logo'}/>
+            <MenuIcon onClick={() => setOpen(!open)} style={{height: 50, width: 50}}/>
+            <Image height={50} src={logoSvg} alt={'logo'}/>
         </div>
 
 
