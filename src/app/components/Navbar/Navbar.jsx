ebdbaf8ead868cce136137cfb2179000bf7ca788
adisb200 <165292754+adisb200@users.noreply.github.com>
2025-01-12 20:41:58 +0100

    Rework Navbar (#12)

--- src/app/components/Navbar/Navbar.jsx
+++ src/app/components/Navbar/Navbar.jsx
@@ -10,18 +10,25 @@ import logoSvg from '../../../assets/images/logo.svg'
 export default function Navbar() {
     const [open, setOpen] = useState(false);
 
-    return (
-        <>
-            <div style={{display:"flex", justifyContent:"space-between"}}>
-                <MenuIcon onClick={() => setOpen(!open)}/>
-                <Image height={20} src={logoSvg} alt={'logo'}/>
-            </div>
+    const links = [
+        {href: '/', label: 'Home'},
+        {href: '/pages/showticket', label: 'Tickets anzeigen'},
+        {href: '/pages/map', label: 'Karten & Route'},
+        {href: '/pages/buyticket', label: 'Tickets kaufen'}
+    ];
 
 
-            {open && <div className={styles.flexColumn}>
-                <Link className={styles.link} href={"/"}>Tickets anzeigen</Link>
-                <Link className={styles.link} href={"/"}>Karten & Route</Link>
-                <Link className={styles.link} href={"/"}>Tickets kaufen</Link>
-            </div>}
-        </>)
+    return (<div className={styles.background}>
+        <div style={{display: "flex", justifyContent: "space-between"}}>
+            <MenuIcon onClick={() => setOpen(!open)} style={{height: 50, width: 50}}/>
+            <Image height={50} src={logoSvg} alt={'logo'}/>
+        </div>
+
+
+        {open && <div className={styles.navbar}>
+            {links.map(({href, label}) => (
+                <Link onClick={() => setOpen(!open)} key={href} href={href} className={styles.link}>{label}</Link>))}
+        </div>}
+
+    </div>)
 }
